b6315b69d08aaf82d620e7911d22a81ba90600ab
HungLuke <luuphuochung04@gmail.com>
2023-05-26 12:02:44 +0700

    Fix HyperLink for all pages

---
+++ Models/Image.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealEstate.Models;
+
+public partial class Image
+{
+    public int Id { get; set; }
+
+    public int? RealEstateId { get; set; }
+
+    public string Image1 { get; set; } = null!;
+
+    public int? SellerId { get; set; }
+
+    public int? NewsId { get; set; }
+
+    public int? LoginUserId { get; set; }
+
+    public virtual LoginUser? LoginUser { get; set; }
+
+    public virtual News? News { get; set; }
+
+    public virtual Property? RealEstate { get; set; }
+
+    public virtual Seller? Seller { get; set; }
+}
---
+++ Models/LoginUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealEstate.Models;
+
+public partial class LoginUser
+{
+    public int Id { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string PassWord { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    /// <summary>
+    /// 0: inactive 
+    /// 1: active
+    /// </summary>
+    public int Status { get; set; }
+
+    public virtual ICollection<Image> Images { get; set; } = new List<Image>();
+}
